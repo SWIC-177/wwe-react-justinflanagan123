369e19b66fe7c98dc082a0e64eb98474c455149e
justinflangan123 <justin.flanagan@student.swic.edu>
2024-05-15 21:42:35 -0500

    Update code

--- src/app.jsx
+++ src/app.jsx
@@ -5,8 +5,8 @@ export default function App() {
     <Table
       col1={["item1", "item2", "item3"]}
       col2={["stuff1", "stuff2", "stuff3"]}
-      col1Header={"Stuff"}
-      col2Header="More STuff"
+      col1Header="Stuff"
+      col2Header="More Stuff"
     />
   );
 }
--- src/components/table.jsx
+++ src/components/table.jsx
@@ -1,25 +1,21 @@
 export default function Table({ col1, col2, col1Header, col2Header }) {
-  return `
-      <table class="container mx-auto text-left max-w-fit">
-        <thead>
-          <tr>
-            <th>${col1Header}</th>
-            <th>${col2Header}</th>
+  return (
+    <table className="container mx-auto max-w-fit text-left">
+      <thread>
+        <tr>
+          <th>{col1Header}</th>
+          <th>{col2Header}</th>
+        </tr>
+      </thread>
+
+      <tbody>
+        {col1.map((item, index) => (
+          <tr key={index}>
+            <td>{item}</td>
+            <td>{col2[index] || ""} </td>
           </tr>
-        </thead>
-  
-        <tbody>
-          ${col1
-            .map(
-              (item, index) => `
-            <tr>
-              <td>${item}</td>
-              <td>${col2[index] || ""}</td>
-            </tr>
-          `,
-            )
-            .join("")}
-        </tbody>
-      </table>
-    `;
+        ))}
+      </tbody>
+    </table>
+  );
 }
