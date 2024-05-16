df93fe511a52eadbf8bb26f9df6787e57a6ae140
justinflangan123 <justin.flanagan@student.swic.edu>
2024-05-15 21:33:04 -0500

    feat: Add App component with Table usage

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,12 @@
 import Table from "./components/table";
 
 export default function App() {
-  return <Table />;
+  return (
+    <Table
+      col1={["item1", "item2", "item3"]}
+      col2={["stuff1", "stuff2", "stuff3"]}
+      col1Header={"Stuff"}
+      col2Header="More STuff"
+    />
+  );
 }
