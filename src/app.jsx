6870680b705d2c5e025630482ea5c651b015cbd1
justinflangan123 <justin.flanagan@student.swic.edu>
2024-05-15 20:42:20 -0500

    feat: Import table componet

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,5 @@
+import Table from "./components/table";
+
 export default function App() {
-  return (
-    <div className="bg-gray-100 p-4">
-      <h1 className="text-center text-2xl font-bold text-gray-800">
-        Hello, Tailwind CSS!
-      </h1>
-    </div>
-  );
+  return <Table />;
 }
